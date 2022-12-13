211e0498c005cb2082ff11eb01cfef267f21749f
amit-momin <108959691+amit-momin@users.noreply.github.com>
2022-12-13 17:17:28 -0600

    Fix/Config number parsing (#11)
    * Fixed config number parsing and adjusted some config limits
    
    * Added integer config tests

--- src/config/index.ts
+++ src/config/index.ts
@@ -223,13 +223,13 @@ export const BaseSettings = {
       'Maximum amount of time (in ms) an SSE subscription will be cached before being unsubscribed',
     type: 'number',
     default: 300000,
-    validate: validator.integer({ min: 0, max: 300000 }),
+    validate: validator.integer({ min: 0, max: 3600000 }),
   },
   WARMUP_SUBSCRIPTION_TTL: {
     type: 'number',
     description: 'TTL for batch warmer subscriptions',
     default: 300000,
-    validate: validator.integer({ min: 0, max: 300000 }),
+    validate: validator.integer({ min: 0, max: 3600000 }),
   },
   // WARMUP_UNHEALTHY_THRESHOLD: {
   //   type: 'number',
@@ -239,7 +239,7 @@ export const BaseSettings = {
     description: 'The time in ms a request will live in the subscription set before becoming stale',
     type: 'number',
     default: 120000,
-    validate: validator.integer({ min: 0, max: 120000 }),
+    validate: validator.integer({ min: 0, max: 3600000 }),
   },
   WS_SUBSCRIPTION_UNRESPONSIVE_TTL: {
     description:
@@ -430,7 +430,7 @@ export const getEnv = (name: string, config: Setting, prefix = ''): SettingValue
     case 'string':
       return value
     case 'number':
-      return parseInt(value)
+      return Number(value)
     case 'boolean':
       return value === 'true'
     case 'enum':
