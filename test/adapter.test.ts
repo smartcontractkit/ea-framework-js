465760379e1b020cb83c0bdd56c152e65ffd5398
karen-stepanyan <karen.stepanyan@protofire.io>
2023-03-29 21:35:34 +0400

    enforce uppercase ea name

--- test/adapter.test.ts
+++ test/adapter.test.ts
@@ -25,3 +25,21 @@ test('duplicate endpoint names throw error on startup', async (t) => {
     message: 'Duplicate endpoint / alias: "test"',
   })
 })
+
+test('lowercase adapter name throws error on startup', async (t) => {
+  const adapter = new Adapter({
+    // @ts-expect-error - tests that lowercase names throw errors in runtime
+    name: 'test',
+    endpoints: [
+      new AdapterEndpoint({
+        name: 'test',
+        inputParameters: {},
+        transport: new NopTransport(),
+      }),
+    ],
+  })
+
+  await t.throwsAsync(async () => expose(adapter), {
+    message: 'Adapter name must be uppercase',
+  })
+})
