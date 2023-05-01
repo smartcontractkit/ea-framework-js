2e4817e84a3bed7bd4c1919a5a01ade98c519a5a
Finley Decker <finleydecker@gmail.com>
2023-05-01 10:43:14 -1000

    linted code

--- scripts/ea-settings-table.ts
+++ scripts/ea-settings-table.ts
@@ -5,23 +5,27 @@ let output = `# EA Settings\n\n|Name|Type|Default|Description|Validation|Min|Max
 const sortedSettings = Object.keys(BaseSettingsDefinition).sort()
 
 for (const settingName of sortedSettings) {
-    const data = BaseSettingsDefinition[settingName as keyof typeof BaseSettingsDefinition]
-    const settingDefault = data['default' as keyof typeof data]
-    
-    let details = ''
-    let min: number | string = ''
-    let max: number | string = ''
+  const data = BaseSettingsDefinition[settingName as keyof typeof BaseSettingsDefinition]
+  const settingDefault = data['default' as keyof typeof data]
 
-    if ('validate' in data) {
-        if (data.validate.meta.details) { 
-            details = `- ${data.validate.meta.details.split(', ').join('<br> - ')}` 
-        }
-        
-        if (typeof data.validate.meta.min === 'number') { min = data.validate.meta.min }
-        if (typeof data.validate.meta.max === 'number') { max = data.validate.meta.max }
+  let details = ''
+  let min: number | string = ''
+  let max: number | string = ''
+
+  if ('validate' in data) {
+    if (data.validate.meta.details) {
+      details = `- ${data.validate.meta.details.split(', ').join('<br> - ')}`
     }
-    
-    output += `|${settingName}|${data.type}|${settingDefault}|${data.description}|${details}|${min}|${max}\n`
+
+    if (typeof data.validate.meta.min === 'number') {
+      min = data.validate.meta.min
+    }
+    if (typeof data.validate.meta.max === 'number') {
+      max = data.validate.meta.max
+    }
+  }
+
+  output += `|${settingName}|${data.type}|${settingDefault}|${data.description}|${details}|${min}|${max}\n`
 }
 
 // eslint-disable-next-line no-console
