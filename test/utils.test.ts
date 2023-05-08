a7c36faaf251385a11fc43c3fa9a600fce3bfa77
karen-stepanyan <karen.stepanyan@protofire.io>
2023-05-08 17:01:37 +0400

    run prettier

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -4,7 +4,7 @@ import test from 'ava'
 test('Test splitArrayIntoChunks function', async (t) => {
   const array = [1, 2, 3, 4, 5]
   let chunks = splitArrayIntoChunks(array, 2)
-  t.deepEqual(chunks, [[1,2], [3,4], [5]])
+  t.deepEqual(chunks, [[1, 2], [3, 4], [5]])
 
   // Size greater than array length returns whole array
   chunks = splitArrayIntoChunks(array, 10)
@@ -12,23 +12,28 @@ test('Test splitArrayIntoChunks function', async (t) => {
 
   chunks = splitArrayIntoChunks([], 10)
   t.deepEqual(chunks, [[]])
-
 })
 
-
 test('Test groupArrayByKey function', async (t) => {
-  const array = [{base: 'BTC', quote: 'ETH'}, {base: 'BTC', quote: 'USD'}, {base: 'LTC', quote: 'DASH'}]
+  const array = [
+    { base: 'BTC', quote: 'ETH' },
+    { base: 'BTC', quote: 'USD' },
+    { base: 'LTC', quote: 'DASH' },
+  ]
   let grouped = groupArrayByKey(array, 'base')
   t.deepEqual(grouped, {
-    BTC: [{base: 'BTC', quote: 'ETH'}, {base: 'BTC', quote: 'USD'}],
-    LTC: [{base: 'LTC', quote: 'DASH'}]
+    BTC: [
+      { base: 'BTC', quote: 'ETH' },
+      { base: 'BTC', quote: 'USD' },
+    ],
+    LTC: [{ base: 'LTC', quote: 'DASH' }],
   })
 
   grouped = groupArrayByKey(array, 'quote')
 
   t.deepEqual(grouped, {
-    ETH: [{base: 'BTC', quote: 'ETH'}],
-    USD: [{base: 'BTC', quote: 'USD'}],
-    DASH: [{base: 'LTC', quote: 'DASH'}]
+    ETH: [{ base: 'BTC', quote: 'ETH' }],
+    USD: [{ base: 'BTC', quote: 'USD' }],
+    DASH: [{ base: 'LTC', quote: 'DASH' }],
   })
 })
