e9ea63337d342af38f187cbb1d630ba9b0f65af5
Finley Decker <finleydecker@gmail.com>
2023-05-23 15:15:38 -1000

    created ErrorWithContext type

--- src/validation/error.ts
+++ src/validation/error.ts
@@ -10,6 +10,15 @@ type ErrorFull = ErrorBasic & {
   cause: string
 }
 
+export type ErrorWithContext = {
+  error: {
+    name: string
+    stack: string | undefined
+    message: string
+  }
+  reqBody?: unknown
+}
+
 export type AdapterErrorResponse = {
   status: string
   statusCode: number
