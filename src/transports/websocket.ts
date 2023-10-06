123107f3de57fa4df495354347e59257cedf8bef
Matthew McAllister <matthew.mcallister@smartcontract.com>
2023-10-06 17:27:32 -0400

    review fix to simplify change

--- src/transports/websocket.ts
+++ src/transports/websocket.ts
@@ -327,11 +327,19 @@ export class WebSocketTransport<
 
     // Check if we should close the current connection
     if (!connectionClosed && (urlChanged || connectionUnresponsive)) {
-      const reason = urlChanged
-        ? `Websocket url has changed from ${this.currentUrl} to ${urlFromConfig}, closing connection...`
-        : `Last message was received ${timeSinceLastMessage} ago, exceeding the threshold of ${context.adapterSettings.WS_SUBSCRIPTION_UNRESPONSIVE_TTL}ms, closing connection...`
-      const logfn = urlChanged && this.isUrlSubscriptionWebsocket ? logger.debug : logger.info
-      censorLogs(() => logfn(reason))
+      if (urlChanged) {
+        censorLogs(() =>
+          logger.debug(
+            `Websocket url has changed from ${this.currentUrl} to ${urlFromConfig}, closing connection...`,
+          ),
+        )
+      } else {
+        censorLogs(() =>
+          logger.info(
+            `Last message was received ${timeSinceLastMessage} ago, exceeding the threshold of ${context.adapterSettings.WS_SUBSCRIPTION_UNRESPONSIVE_TTL}ms, closing connection...`,
+          ),
+        )
+      }
 
       // Check if connection was opened very recently; if so, wait a bit before continuing.
       // This is so if we just opened the connection and are waiting to receive some messages,
@@ -466,14 +474,5 @@ export class WebsocketReverseMappingTransport<
    */
   getReverseMapping(value: K): TypeFromDefinition<T['Parameters']> | undefined {
     return this.requestMapping.get(value)
-  }
-}
-
-export class UrlSubscriptionWebsocketTransport<
-  T extends WebsocketTransportGenerics,
-> extends WebSocketTransport<T> {
-  constructor(config: WebSocketTransportConfig<T>) {
-    super(config)
-    this.isUrlSubscriptionWebsocket = true
   }
 }
